d57640c09c6c94f873b3306c7a25db7f940c5a48
paulohenrique-souza-dev <paulo.ricklima788@gmail.com>
2025-08-06 13:26:47 -0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views
 
 from accounts.views import RegisterView
 
+
+## aqui sao as urls de login,cadastro e logout aqui que aponta a rota/urls.
 urlpatterns=[
     path('accounts/register/',RegisterView.as_view(),name='register'),
     path('accounts/login/',views.LoginView.as_view(),name='login'),
